166ab65486e6821ff7c8664e0fe099082e25253a
Geert Bevin <gbevin@uwyn.com>
2023-01-01 22:11:51 -0500

    Try to integrate with tests badge service

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,7 +113,7 @@ tasks {
                                 HttpRequest.newBuilder()
                                     .uri(
                                         URI(
-                                            "https://rife2.com/testbadge/update/com.uwyn.rife2/tests-badge?" +
+                                            "https://rife2.com/tests-badge/update/com.uwyn.rife2/tests-badge?" +
                                                     "apiKey=$apiKey&" +
                                                     "passed=$passed&" +
                                                     "failed=$failed&" +
