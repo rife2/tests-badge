ff46bf0ab127b576fe175d94e4e6ac7e974aba66
Geert Bevin <gbevin@uwyn.com>
2023-03-07 14:55:51 -0500

    Allow virtual threads to be enabled with RIFE2 updated implementation

--- app/src/main/java/com/uwyn/testsbadge/TestsBadgeSiteUber.java
+++ app/src/main/java/com/uwyn/testsbadge/TestsBadgeSiteUber.java
@@ -5,7 +5,6 @@ import rife.engine.*;
 public class TestsBadgeSiteUber extends TestsBadgeSite {
     public static void main(String[] args) {
         new Server()
-            .enabledVirtualThreads(false)
             .staticUberJarResourceBase("webapp")
             .start(new TestsBadgeSite());
     }
