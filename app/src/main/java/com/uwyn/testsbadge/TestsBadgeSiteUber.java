f5709b29915aa671fbdc1862e3ee1a79a822e42d
Geert Bevin <gbevin@uwyn.com>
2023-03-07 15:11:38 -0500

    Disable virtual threads again, still seems to be an issue with native image.

--- app/src/main/java/com/uwyn/testsbadge/TestsBadgeSiteUber.java
+++ app/src/main/java/com/uwyn/testsbadge/TestsBadgeSiteUber.java
@@ -5,6 +5,7 @@ import rife.engine.*;
 public class TestsBadgeSiteUber extends TestsBadgeSite {
     public static void main(String[] args) {
         new Server()
+            .enabledVirtualThreads(false)
             .staticUberJarResourceBase("webapp")
             .start(new TestsBadgeSite());
     }
