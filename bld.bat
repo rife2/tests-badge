87867701aac5152ae9136f31b8e0dd96b94eaaee
Geert Bevin <gbevin@uwyn.com>
2023-03-19 12:37:14 -0400

    Updated to latest build tool

--- bld.bat
+++ bld.bat
@@ -1,4 +1,4 @@
 @echo off
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
-java -jar "%DIRNAME%/lib/bld/bld-wrapper.jar" "%0" src/bld/java/com/uwyn/testsbadge/TestsBadgeBuild.java %*
+java -jar "%DIRNAME%/lib/bld/bld-wrapper.jar" "%0" --build com.uwyn.testsbadge.TestsBadgeBuild %*
