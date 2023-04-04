68e9a19b1cbd99ac52969409a9d2bb25176147ed
Geert Bevin <gbevin@uwyn.com>
2023-04-04 17:29:01 -0400

    Updated dependencies and cleanups

--- src/bld/java/com/uwyn/testsbadge/TestsBadgeBuild.java
+++ src/bld/java/com/uwyn/testsbadge/TestsBadgeBuild.java
@@ -6,7 +6,10 @@ package com.uwyn.testsbadge;
 
 import rife.bld.WebProject;
 import rife.bld.extension.TestsBadgeOperation;
+import rife.bld.publish.PublishDeveloper;
 import rife.bld.publish.PublishInfo;
+import rife.bld.publish.PublishLicense;
+import rife.bld.publish.PublishScm;
 
 import java.util.List;
 
@@ -20,14 +23,28 @@ public class TestsBadgeBuild extends WebProject {
         name = "TestsBadge";
         mainClass = "com.uwyn.testsbadge.TestsBadgeSite";
         uberJarMainClass = "com.uwyn.testsbadge.TestsBadgeSiteUber";
-        version = version(1,4,2);
+        version = version(1,4,3);
 
         precompiledTemplateTypes = List.of(HTML, SVG, JSON);
 
-        publishRepository = repository("rife2");
+        publishRepository = version.isSnapshot() ? repository("rife2-snapshots") : repository("rife2-releases");
         publishInfo = new PublishInfo()
             .groupId("com.uwyn")
             .artifactId("testsbadge")
+            .description("Status badge that reports the number of passed and failed tests in your project.")
+            .url("https://github.com/rife2/tests-badge")
+            .developer(new PublishDeveloper()
+                .id("gbevin")
+                .name("Geert Bevin")
+                .email("gbevin@uwyn.com")
+                .url("https://github.com/gbevin"))
+            .license(new PublishLicense()
+                .name("The Apache License, Version 2.0")
+                .url("https://www.apache.org/licenses/LICENSE-2.0.txt"))
+            .scm(new PublishScm()
+                .connection("scm:git:https://github.com/rife2/tests-badge.git")
+                .developerConnection("scm:git:git@github.com:rife2/tests-badge.git")
+                .url("https://github.com/rife2/tests-badge"))
             .signKey(property("sign.key"))
             .signPassphrase(property("sign.passphrase"));
 
@@ -36,7 +53,7 @@ public class TestsBadgeBuild extends WebProject {
         autoDownloadPurge = true;
         repositories = List.of(MAVEN_CENTRAL, RIFE2);
         scope(compile)
-            .include(dependency("com.uwyn.rife2", "rife2", version(1,5,16)));
+            .include(dependency("com.uwyn.rife2", "rife2", version(1,5,17)));
         scope(runtime)
             .include(dependency("org.postgresql", "postgresql", version(42,6,0)))
             .include(dependency("com.h2database", "h2", version(2,1,214)));
@@ -51,12 +68,14 @@ public class TestsBadgeBuild extends WebProject {
             .include(dependency("org.slf4j", "slf4j-simple", version(2,0,7)));
     }
 
+    private final TestsBadgeOperation testsBadgeOperation = new TestsBadgeOperation();
+
     public void test()
     throws Exception {
-        new TestsBadgeOperation()
+        testsBadgeOperation.executeOnce(() -> testsBadgeOperation
             .url(property("testsBadgeUrl"))
             .apiKey(property("testsBadgeApiKey"))
-            .fromProject(this).execute();
+            .fromProject(this));
     }
 
     public static void main(String[] args) {
